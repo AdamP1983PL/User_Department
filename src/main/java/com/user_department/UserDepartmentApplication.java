949a8dd3860a42a3ae174411d1d60a0f03039860
Adam Podlaski <podlaski83@gmail.com>
2024-03-11 10:24:22 +0100

    initial commit

---
+++ src/main/java/com/user_department/UserDepartmentApplication.java
@@ -0,0 +1,16 @@
+package com.user_department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:database.properties")
+
+public class UserDepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDepartmentApplication.class, args);
+	}
+
+}
